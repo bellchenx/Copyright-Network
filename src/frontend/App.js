b59e9df52b247147f889b2c19f1659f9da143c2f
gab3-me <107727960+gab3-me@users.noreply.github.com>
2022-06-25 18:41:39 -0400

    Changed Dashboard to Canvas

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,56 +1,10 @@
-import React, {useEffect} from 'react';
-import{TooltipComponent} from '@syncfusion/ej2-react-popups';
-import {FiSettings} from 'react-icons/fi';
-
-import {Navbar, Sidebar} from './components';
-import {Canvas, Home} from './pages';
-import './App.css';
-
-import { useStateContext} from './contexts/ContextProvider'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import OverviewFlow from './OverviewFlow'
 
 const App = () => {
-  const {activeMenu} = useStateContext();
   return (
-    <div>
-      <BrowserRouter>
-        <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4"
-            style={{zIndex: '1000'}}
-          >
-            <TooltipComponent content="Settings" position="Top">
-              <button type="button"
-                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                style={{background: 'blue', borderRadius: '50%'}}
-              >
-                <FiSettings/>
-              </button>
-            </TooltipComponent>
-          </div>
-          {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              <Sidebar/>
-            </div>
-          ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">
-              <Sidebar/>
-            </div>
-          )}
-          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              <Navbar/>
-            </div>
-            <div>
-              <Routes>
-                {/* Dashboard */}
-                <Route path = "/" element={<Home/>}/>
-                {/* Apps */}
-                <Route path = "/canvas" element={<Canvas/>}/>
-              </Routes>
-            </div>
-          </div>
-        </div>
-      </BrowserRouter>
+    <div style={{height: 800}}>
+        <OverviewFlow/>
     </div>
   )
 }
